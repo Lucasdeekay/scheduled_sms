ea854bad9ca48ba00ec7a52bd520e1cffd9e39af
Lucasdeekay <lucasdeekay98@gmail.com>
2025-12-24 15:51:07 +0100

    Updated project

--- api/models.py
+++ api/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from django.utils import timezone
+import pytz
 
+# WAT is UTC+1
+wat_tz = pytz.timezone('Africa/Lagos')
 phone_validator = RegexValidator(
     regex=r'^\+[1-9]\d{1,14}$',
     message="Phone must be in E.164 format (+1234567890)"
 )
-
 
 class Message(models.Model):
     sender_name   = models.CharField(max_length=100)
     receiver_name = models.CharField(max_length=100)
     receiver_phone = models.CharField(max_length=20, validators=[phone_validator])
     message       = models.TextField(max_length=1600)
-    scheduled_time = models.DateTimeField()
+    scheduled_time = models.DateTimeField(
+        default=timezone.datetime(2025, 12, 24, 16, 0, tzinfo=wat_tz)
+    )
     sent_at       = models.DateTimeField(null=True, blank=True)
     created_at    = models.DateTimeField(auto_now_add=True)
 
